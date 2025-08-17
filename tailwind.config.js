57759f71e50010cbc1220d3059486de72b0d68b8
Ifeoluwayemisi <destinifeoluwa@gmail.com>
2025-08-17 13:40:56 +0100

    Did some modication

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./*.html", "./assets/js/**/*.js"],
   theme: {
     extend: {
-    
+      fontSize: {
+        '10xl': '10rem',
+        '20xl': '20rem',
+      }
     },
   },
   plugins: [],
